9e96708bf91f708fa7cff7fcecfda7cc3e512cb4
Trishant Pahwa <trishantpahwa@gmail.com>
2020-10-07 17:22:09 +0530

    Initialized project.

---
+++ src/Routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const model = require('../Models/index.js');
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+
+});
+
+module.exports = router;
